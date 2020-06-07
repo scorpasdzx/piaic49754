9510fe54bc91faed7f1e09f476799eac734f4fa6
Wajid Ali <wajidali1987@gmail.com>
2020-06-07 22:43:21 +0500

    Initial commit

---
+++ src/Hi.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './Hi.css';
+
+function Hi() {
+  return <div className="App-header"> Hello <strong>Wajid Ali</strong><p>Lets see how an unordered list is rendered</p>
+  <ul className="App-link">
+    <li>Cars</li>
+    <li>Bikes</li>
+    <li>AeroPlanes</li>
+    <li>{5+10}</li>
+    </ul>
+    </div>;
+}
+
+export default Hi;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Hi from './Hi';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  //<React.StrictMode>
+    <Hi/>,
+  //</React.StrictMode>,
   document.getElementById('root')
 );
 
